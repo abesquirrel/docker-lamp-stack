2327a6fd63aa1274153167e28315a34f08e80723
Paul Rojas <paul.henry.rojas@gmail.com>
2022-10-09 18:27:37 +0300

    50-server.cnf - created config file to replace default mariadb config.
    docker-compose.yml
    - format the global variables to prevent future problems due to naming conventions.
    
    Dockerfile
    - inserted back docker-php-ext-xdebug.ini, probably erased by mistake.
    
    index.php
    - add informative lines that also help to check everything is properly running.

--- www/index.php
+++ www/index.php
@@ -1,5 +1,21 @@
 <?php
+echo "OS: ".php_uname('s').'<br>';
+echo "Apache version: ".apache_get_version()."<br>";
+echo "PHP version: ".phpversion()."<br>";
 
-echo apache_get_version();
+$dbname = 'test';
+$dbuser = 'paul';
+$dbpass = 'root';
+$dbhost = '172.17.0.1';
 
-phpinfo();
+$link = mysqli_connect($dbhost, $dbuser, $dbpass);
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/* print server version */
+printf("Server version: %s\n", mysqli_get_server_info($link));
+/* close connection */
+mysqli_close($link);
